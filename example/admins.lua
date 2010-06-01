2a6d7bd370fd1aebb3530666a5ba2bd7fef7111d
Jakob <jakobovrum@gmail.com>
2010-06-01 02:07:35 +0200

    Restored config.lua in example

--- example/admins.lua
+++
@@ -1,5 +0,0 @@
---list of accounts with admin rights
-admins
-{
-	"jA_cOp"; --replace with your IRC account name. Case-sensitive, unlike IRC.
-}
---
+++ example/config.lua
@@ -0,0 +1,17 @@
+nick = "replaceme"
+username = "lua"
+realname = "replaceme"
+
+server = "replaceme"
+
+channels
+{
+	"#replaceme";
+}
+
+admins
+{
+	"replaceme"; --Server authentication service name, usually just your registered nick
+}
+
+plugin_dir = "plugins"
