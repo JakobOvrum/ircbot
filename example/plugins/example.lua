9e513be9904d9d47b111a5d9d28a1db34aa9754e
Jakob Ovrum <jakobovrum@gmail.com>
2011-02-11 16:56:10 +0900

    Expanded example plugin

--- example/plugins/example.lua
+++ example/plugins/example.lua
@@ -17,17 +17,17 @@ Command "echo"
 Hook "OnJoin"
 {
 	function(user, channel)
-		send{target = channel, message = "Welcome!"}
+		send{target = channel, message = string.format("Welcome to %s, %s!", channel, user.nick)}
 	end
 }
 
 --Grabs select information from passed text using patterns
 Command "extract"
 {
-	expectedArgs = "Once upon a (%S+) I walked this (%S+)";
+	expectedArgs = "[Oo]nce upon a (%S+) I (%S+) this (%S+)";
 
-	function(a, b)
-		reply("%s: Once upon a %s you walked this %s", user.nick, a, b)
+	function(a, b, c)
+		reply("%s: Once upon a %s you %s this %s", user.nick, a, b, c)
 	end
 }
 
@@ -51,3 +51,56 @@ Command "secret" "very-secret" "super-secret"
 	end
 }
 
+--Count all arguments passed to the command
+Command "wordcount"
+{
+	expectedArgs = -1; --accept any number of arguments
+	function(...)
+		reply("You passed %d arguments", select("#", ...))
+	end
+}
+
+--Print a line to the bot log
+Command "log"
+{
+	function(line)
+		log("Log command: %s", line)
+	end
+}
+
+--Say "Hi!", wait 3 seconds, say "I'm a bot!", wait 10 seconds, repeat
+do
+	local spamchannel
+	
+	--Think hooks are run once every bot:think(), but can be delayed with the "wait" function.
+	--The "wait" function does not block the rest of the program.
+	Hook "Think"
+	{
+		enabled = false; --not enabled by default
+
+		function()
+			send{target = spamchannel, message = bold("Hi!")}
+			wait(3)
+			send{target = spamchannel, message = underline("I'm a bot!")}
+			wait(10)
+		end
+	}
+
+	--Enable or disable the previous example
+	Command "greeter"
+	{
+		function(cmd)
+			if cmd == "enable" then
+				enableThink()
+				spamchannel = channel
+				reply("Enabled greeter.")
+			elseif cmd == "disable" then
+				disableThink()
+				spamchannel = nil
+				reply("Disabled greeter.")
+			else
+				reply("Argument must be 'enable' or 'disable'.")
+			end
+		end
+	}
+end
